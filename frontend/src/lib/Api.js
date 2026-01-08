643d61caa6b6131242bfced3b97a6a51c615a879
Tejasvi-Chauhan <chauhantejasvi07@gmail.com>
2026-01-08 16:08:12 +0530

    routing

--- frontend/src/lib/Api.js
+++ frontend/src/lib/Api.js
@@ -3,7 +3,7 @@ import axios from "axios";
  
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: "https://notes-taking-app-ejkn.onrender.com",
   withCredentials: true,
 });
 
